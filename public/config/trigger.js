10cddbed48bb308c5edd1bfd271927e91b43a929
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2019-10-25 14:05:40 -0400

    fix: electron rearrange

---
+++ public/config/trigger.js
@@ -0,0 +1,18 @@
+// Event trigger settings - used in both the react app (renderer) and the electron app (main)
+const manufacturer = 'Teensyduino'
+const vendorId = '16c0'
+const productId = '0487'
+
+const eventCodes = {
+	fixation: 1,
+	evidence: 5,
+	show_earnings: 7
+}
+
+// this is module.exports isntead of just exports as it is also imported into the electron app
+module.exports = {
+	manufacturer,
+	vendorId,
+	productId,
+	eventCodes
+}
