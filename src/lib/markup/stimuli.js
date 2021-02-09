45282119011b71c4bef937510ec8220caeddd29c
Nicole Provenza <nicole_provenza@brown.edu>
2021-02-08 19:34:18 -0500

    removed beads references
    Changed 'beads_container' to 'center_container' and deleted moneybags images in assets folder

--- src/lib/markup/stimuli.js
+++ src/lib/markup/stimuli.js
@@ -1,7 +1,7 @@
 const baseStimulus = (element, prompt=false) => {
   const class_ = (prompt) ? 'main-prompt': 'main'
   return (
-    `<div class='beads_container'>
+    `<div class='center_container'>
     <header>
 	  <h1>${element}</h1>
     </header>
