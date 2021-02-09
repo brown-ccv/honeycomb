3bbf40493a671e97e151bb91f6f22a0aee39f82a
Nicole Provenza <nicole_provenza@brown.edu>
2021-02-08 19:27:15 -0500

    Update stimuli.js
    centered stimuli and made it bigger (a header)

--- src/lib/markup/stimuli.js
+++ src/lib/markup/stimuli.js
@@ -1,6 +1,12 @@
 const baseStimulus = (element, prompt=false) => {
   const class_ = (prompt) ? 'main-prompt': 'main'
-  return `<div class=${class_}>${element}</div>`
+  return (
+    `<div class='beads_container'>
+    <header>
+	  <h1>${element}</h1>
+    </header>
+    </div>`
+  )
 }
 
 
