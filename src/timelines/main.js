ef4c3283d2f7a9e3773865822936161ad4de7eb8
Rashi1997 <rdhar1997@gmail.com>
2021-02-11 14:28:03 -0500

    refactor: 💡 reflect the changes in task library

--- src/timelines/main.js
+++ src/timelines/main.js
@@ -9,9 +9,9 @@ import { exptBlock1, exptBlock2 } from "../config/experiment";
 
 const primaryTimeline = [
   preamble,
-  countdown({ duration: 1000, text: lang.countdown.message1, time: 3 }),
+  countdown({ duration: 1000, message: lang.countdown.message1, time: 3 }),
   taskBlock(practiceBlock),
-  countdown({ duration: 1000, text: lang.countdown.message2, time: 3 }),
+  countdown({ duration: 1000, message: lang.countdown.message2, time: 3 }),
   taskBlock(exptBlock1),
   showMessage(config, {
     responseType: "html_keyboard_response",
@@ -22,9 +22,9 @@ const primaryTimeline = [
 
 const mturkTimeline = [
   preamble,
-  countdown({ duration: 1000, text: lang.countdown.message1, time: 3 }),
+  countdown({ duration: 1000, message: lang.countdown.message1, time: 3 }),
   taskBlock(tutorialBlock),
-  countdown({ duration: 1000, text: lang.countdown.message2, time: 3 }),
+  countdown({ duration: 1000, message: lang.countdown.message2, time: 3 }),
   taskBlock(exptBlock2),
   showMessage(config, {
     responseType: "html_keyboard_response",
