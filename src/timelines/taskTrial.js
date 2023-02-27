6913170ee45c9fbfdb275d3ef38a9cd747cdd8c7
RobertGemmaJr <rgemma.jr@gmail.com>
2023-02-27 10:51:26 -0500

    feat(taskTrial): Add changes from PR 88

--- src/timelines/taskTrial.js
+++ src/timelines/taskTrial.js
@@ -1,36 +1,30 @@
-import htmlKeyboardResponse from '@jspsych/plugin-html-keyboard-response'
-import { showMessage, fixation } from "@brown-ccv/behavioral-task-trials";
-import { config } from "../config/main";
-import { eventCodes } from "../config/main";
-import { earningsDisplay } from "../lib/markup/earnings";
+import { fixation } from "@brown-ccv/behavioral-task-trials";
+import { envConfig } from "../config/main";
+import choice from "../trials/choice"
+import { getRandomInt } from "../lib/utils"
+import showEarnings from "../trials/showEarnings"
 
-const taskTrial = (blockSettings, blockDetails, condition) => {
-  // timeline
+/**
+ * Sets up a Stroop trial.
+ * @param experimentConfig The experiment config object.
+ * @param word The color word to display for this trial.
+ * @returns {any} A jsPsych trial object containing a Stroop trial timeline.
+ */
+const taskTrial = (experimentConfig, word) => {
+  // Set a random font color for the trial, using the colors provided in experimentConfig.
+  const colors = experimentConfig.conditions
+  const color = colors[getRandomInt(colors.length)]
+
   let timeline = [
-    // fixation
-    fixation(config, {
-      duration: 650,
-    }),
-    // show condition
-    showMessage(config, {
-      message: condition,
-      onstart: true,
-      taskCode: eventCodes.evidence,
-    }),
-    fixation(config, {
-      duration: 650,
-    }),
-    // end the trial
-    showMessage(config, {
-      stimulus: earningsDisplay(Math.random()),
-      taskCode: eventCodes.show_earnings,
-    }),
+    // Show the fixation dot.
+    fixation(envConfig, {duration: 650,}),
+    // Display a word and wait for user input.
+    choice(word, color, experimentConfig.response_time),
+    // End the trial by displaying the participant's earnings.
+    showEarnings(1500)
   ];
 
-  return {
-    type: htmlKeyboardResponse,
-    timeline: timeline,
-  };
-};
+  return {type: "html_keyboard_response", timeline};
+}
 
 export default taskTrial;
