7fc72f0913c001c1052e9e80b80f6fac3e30b1c8
RobertGemmaJr <rgemma.jr@gmail.com>
2023-02-27 11:55:41 -0500

    feat(config/main): Merge Add changes from #88

--- src/config/main.js
+++ src/config/main.js
@@ -1,24 +1,62 @@
-// config/main.js
-// This is the main configuration file where universal and default settings should be placed.
-// These settins can then be imported anywhere in the app as they are exported at the botom of the file.
+/**
+ * This is the main configuration file where universal/default settings should be placed.
+ */
 
-import { initJsPsych } from "jspsych";
 import _ from "lodash";
-import { eventCodes } from "./trigger";
+import { initJsPsych } from "jspsych";
 import { init } from "@brown-ccv/behavioral-task-trials";
-import { getProlificId } from "../lib/utils";
+
 import packageInfo from '../../package.json'
+import usLanguage from "../language/en_us.json"
+import mturkLanguage from "../language/en_us.mturk.json"
+import { getProlificId } from "../lib/utils";
+import { eventCodes } from "./trigger";
 
 // Access package name and version so we can store these as facts with task data.
 const taskName = packageInfo.name;
 const taskVersion = packageInfo.version;
 
-// As of jspsych 7, we instantiate jsPsych where needed insead of importing it globally.
+// As of jspsych 7, we instantiate jsPsych where needed instead of importing it globally.
 // The instance here gives access to utils in jsPsych.turk, for awareness of the mturk environment, if any.
 // The actual task and related utils will use a different instance of jsPsych created after login.
 const jsPsych = initJsPsych()
 
-// mapping of letters to key codes
+/** DETERMINE CONFIG FROM ENV */
+
+// Is this mechanical turk?
+const turkInfo = jsPsych.turk.turkInfo()
+const turkUniqueId = `${turkInfo.workerId}:${turkInfo.assignmentId}`
+let USE_MTURK = !turkInfo.outsideTurk;
+
+// Is this prolific?
+let USE_PROLIFIC = getProlificId() && !USE_MTURK;
+
+// Is this electron?
+let USE_ELECTRON = true;
+try {
+  window.require("electron");
+} catch(e) {
+  USE_ELECTRON = false;
+}
+
+// Is this firebase?
+let USE_FIREBASE = process.env.REACT_APP_FIREBASE === "true";
+
+// Use volume trials in the task?
+const USE_VOLUME = process.env.REACT_APP_VOLUME === "true";
+// Use camera trials in the task? (only used with Electron)
+const USE_CAMERA = process.env.REACT_APP_VIDEO === "true" && USE_ELECTRON;
+// Use EEG/event markers? (only used with Electron)
+const USE_EEG =
+  process.env.REACT_APP_USE_EEG === "true" && USE_ELECTRON;
+// Use the photodiode? (only used with Electron)
+const USE_PHOTODIODE =
+  process.env.REACT_APP_USE_PHOTODIODE === "true" && USE_ELECTRON;
+
+
+/** SET OTHER CONFIG */
+
+// Mapping of letters to key codes
 const keys = {
   A: 65,
   B: 66,
@@ -28,46 +66,17 @@ const keys = {
   space: 32,
 };
 
-// audio codes
+// Audio codes
 const audioCodes = {
   frequency: 100 * (eventCodes.open_task - 9),
   type: "sine",
 };
 
-// is this mechanical turk?
-const turkInfo = jsPsych.turk.turkInfo()
-const turkUniqueId = `${turkInfo.workerId}:${turkInfo.assignmentId}`
-let USE_MTURK = !turkInfo.outsideTurk;
-let USE_PROLIFIC = getProlificId() && !USE_MTURK;
-let USE_ELECTRON = true;
-let USE_FIREBASE = process.env.REACT_APP_FIREBASE === "true";
+// Language configuration
+const language = usLanguage
+if (!USE_ELECTRON)  _.merge(language, mturkLanguage);
 
-try {
-  window.require("electron");
-} catch {
-  USE_ELECTRON = false;
-}
-
-// whether or not to ask the participant to adjust the volume
-const USE_VOLUME = process.env.REACT_APP_VOLUME === "true";
-// these variables depend on USE_ELECTRON
-// whether or not to enable video
-const USE_CAMERA = process.env.REACT_APP_VIDEO === "true" && USE_ELECTRON;
-// whether or not the EEG/event marker is available
-const USE_EEG =
-  process.env.REACT_APP_USE_EEG === "true" && USE_ELECTRON;
-// whether or not the photodiode is in use
-const USE_PHOTODIODE =
-  process.env.REACT_APP_USE_PHOTODIODE === "true" && USE_ELECTRON;
-
-// get language file
-const lang = require("../language/en_us.json");
-if (!USE_ELECTRON) {
-  // if this is mturk, merge in the mturk specific language
-  const mlang = require("../language/en_us.mturk.json");
-  _.merge(lang, mlang);
-}
-
+// Get block settings
 const defaultBlockSettings = {
   conditions: ["a", "b", "c"],
   repeats_per_condition: 1, // number of times every condition is repeated
@@ -77,7 +86,7 @@ const defaultBlockSettings = {
 };
 
 // setting config for trials
-const envConfig = init({
+const envConfig = {
   USE_PHOTODIODE,
   USE_EEG,
   USE_ELECTRON,
@@ -86,14 +95,14 @@ const envConfig = init({
   USE_CAMERA,
   USE_PROLIFIC,
   USE_FIREBASE
-});
+}
 
 export {
   taskName,
   taskVersion,
   keys,
   defaultBlockSettings,
-  lang,
+  language,
   eventCodes,
   envConfig,
   audioCodes,
