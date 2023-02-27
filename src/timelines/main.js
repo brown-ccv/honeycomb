b3bfd0fdedcb2ea21e367578a3ccf6755f8dd9fc
RobertGemmaJr <rgemma.jr@gmail.com>
2023-02-27 11:12:22 -0500

    feat(timeline/main.js): Add changes from PR #88

--- src/timelines/main.js
+++ src/timelines/main.js
@@ -1,12 +1,9 @@
-import { lang, config } from "../config/main";
-import preamble from "./preamble";
-import taskBlock from "./taskBlock";
 import { countdown, showMessage } from "@brown-ccv/behavioral-task-trials";
+import { lang, envConfig } from "../config/main";
 import { cameraStart, cameraEnd } from "../trials/camera"
 import { practiceBlock } from "../config/practice";
 import { tutorialBlock } from "../config/tutorial";
 import { exptBlock1, exptBlock2 } from "../config/experiment";
-
 import {
   ageCheck,
   sliderCheck,
@@ -14,60 +11,67 @@ import {
   iusSurvey,
   debrief,
 } from "../trials/quizTrials";
+import preamble from "./preamble";
+import taskBlock from "./taskBlock";
 
+// TODO: PR #88 doesn't use jsPsych options, which is correct?
 // Add your jsPsych options here.
 // Honeycomb will combine these custom options with other options needed by Honyecomb.
-const jsPsychOptions = {
-  on_trial_finish: function (data) {
-    console.log('A trial just ended, here are the latest data:');
-    console.log(data);
-  },
-  default_iti: 250
-};
+// const jsPsychOptions = {
+//   on_trial_finish: function (data) {
+//     console.log('A trial just ended, here are the latest data:');
+//     console.log(data);
+//   },
+//   default_iti: 250
+// };
 
+// TODO: PR #88 doesn't use the MTurk timeline, which is correct?
 // Add your jsPsych timeline here.
 // Honeycomb will call this function for us after the subject logs in, and run the resulting timeline.
 // The instance of jsPsych passed in will include jsPsychOptions above, plus other options needed by Honeycomb.
-const buildTimeline = (jsPsych) => config.USE_MTURK ? mturkTimeline : buildPrimaryTimeline(jsPsych);
+// const buildTimeline = (jsPsych) => config.USE_MTURK ? mturkTimeline : buildPrimaryTimeline(jsPsych);
 
-const buildPrimaryTimeline = (jsPsych) => {
-  let primaryTimeline = [
-    preamble,
+const buildPrimaryTimeline = (experimentConfig) => {
+  // Unconditional part of the timeline
+  let timeline = [
+    preamble(experimentConfig),
     ageCheck,
     sliderCheck,
     countdown({ message: lang.countdown.message1 }),
-    taskBlock(practiceBlock),
-    countdown({ message: lang.countdown.message2 }),
-    taskBlock(exptBlock1),
+    taskBlock(experimentConfig),
     demographics,
     iusSurvey,
-    debrief,
+    debrief
   ];
 
-  if (config.USE_CAMERA) {
-    primaryTimeline.splice(1, 0, cameraStart(jsPsych))
-    primaryTimeline.push(cameraEnd(5000))
+  // Add camera-specific trials to the timeline if in use.
+  if (envConfig.USE_CAMERA) {
+    timeline.splice(1, 0, cameraStart())
+    timeline.push(cameraEnd(5000))
   }
 
-  primaryTimeline.push(showMessage(config, {
+  // Add ending message
+  timeline.push(showMessage(envConfig, {
     duration: 5000,
     message: lang.task.end,
   }))
 
-  return primaryTimeline
+  return timeline
 }
 
-const mturkTimeline = [
-  preamble,
-  countdown({ message: lang.countdown.message1 }),
-  taskBlock(tutorialBlock),
-  countdown({ message: lang.countdown.message2 }),
-  taskBlock(exptBlock2),
-  showMessage(config, {
-    duration: 5000,
-    message: lang.task.end,
-  }),
-];
+// TODO: PR #88 doesn't use the MTurk timeline, which is correct?
+// const mturkTimeline = [
+//   preamble,
+//   countdown({ message: lang.countdown.message1 }),
+//   taskBlock(tutorialBlock),
+//   countdown({ message: lang.countdown.message2 }),
+//   taskBlock(exptBlock2),
+//   showMessage(config, {
+//     duration: 5000,
+//     message: lang.task.end,
+//   }),
+// ];
 
 // Honeycomb, please include these options, and please get the timeline from this function.
-export { jsPsychOptions, buildTimeline };
+// export { jsPsychOptions, buildTimeline };
+export default buildPrimaryTimeline
