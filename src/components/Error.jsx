99d1478f684095838af598820614bb302ddbe34a
RobertGemmaJr <rgemma.jr@gmail.com>
2023-04-19 13:19:19 -0400

    chore: import react with Error component

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,14 +1,15 @@
+import React from 'react';
 
 /** Error
  * This is a basic JSX component for displaying an error
  * By using a separate file the code in App.jsx is easier to read
  */
-export default function Error () {
+export default function Error() {
   return (
     <div className='centered-h-v'>
       <div className='width-50 alert alert-danger'>
         Please ask your task provider to enable firebase.
       </div>
     </div>
-  )
+  );
 }
