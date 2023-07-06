c4af02c0db46cac81bc1222e3e0a17ddbbd1a6c4
RobertGemmaJr <rgemma.jr@gmail.com>
2023-07-06 13:49:37 -0400

    Move forge config from package.json to forge.config.js and update icons

---
+++ webpack.rules.js
@@ -0,0 +1,12 @@
+module.exports = [
+  {
+    test: /\.jsx?$/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        exclude: /node_modules/,
+        presets: ['@babel/preset-react'],
+      },
+    },
+  },
+];
