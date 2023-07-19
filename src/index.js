373d7fedcb8b74f37c7b8192f1a8e79bf896ba60
RobertGemmaJr <rgemma.jr@gmail.com>
2023-07-19 15:18:02 -0400

    feat: Render in StrictMode

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './index.css';
@@ -6,4 +6,8 @@ import App from './components/App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
