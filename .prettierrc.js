aa8af1daa7900afed144a9a6c3a18c76106d8a95
RobertGemmaJr <rgemma.jr@gmail.com>
2023-07-19 15:19:37 -0400

    style: UPdate prettierrc to use double quotes instead of singles

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,8 +4,6 @@
  */
 module.exports = {
   trailingComma: 'es5', // Add a trailing comma to all es5 modules
-  singleQuote: true, // Use single quote (') for strings instead of doubles ("")
-  jsxSingleQuote: true, // Use single quote (') for strings instead of doubles ("") in JSX files
   printWidth: 100, // Sets the maximum line size to 100 (default is 80)
   endOfLine: 'auto', // Configure the end of line character (/n) | REQUIRED FOR OS COMPATIBILITY
 };
