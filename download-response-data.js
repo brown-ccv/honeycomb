aeb9f67a39a46dd5c6c9d23821957e94f2f84303
RobertGemmaJr <rgemma.jr@gmail.com>
2023-08-04 14:52:46 -0400

    chore: Remove local documentation for downloaidng data

--- download-response-data.js
+++ download-response-data.js
@@ -4,63 +4,13 @@
  * Usage:
  *   npm run firebase:download -- studyID participantID [sessionNumber] [outputRoot]
  *
- *   studyID        must be one of the study Ids that a participant can log in with
- *   participantID  must be one of the participant Ids that a participant can log in with
- *   sessionNumber  optional number to select which session to download, defaults to "latest", ie the most recent session
- *   outputRoot     optional path to the folder where data should be saved, defaults to ".", ie the current folder
+ * studyID: The ID of a given study
+ * participantID: The ID of a given participant on the `studyID` study
+ * sessionNumber: Optional number to select which session to download, defaults to the most recent session
+ * outputRoot: Optional path to the folder where data should be saved, defaults to the current folder
  *
- * Before using this, you have to do a little setup.
- *
- * You must be a project owner for the Firebase project that you want to get data from.
- * Then you must set up a service account to allow your local machine to access the data.
- * The official docs for this are here: https://cloud.google.com/docs/authentication/production
- *
- * And here's a summary:
- *  - Visit the Firebase console at https://console.firebase.google.com/
- *  - Click on the Firebase project you want to get data from
- *  - In the top left, click on the gear next to "Project Overview"
- *  - Click on "Users and permissions"
- *  - Click on the "Service accounts" tab
- *  - Near the bottom click "Generate new Private key"
- *  - Save the key on your local machine and remember where you put it (don't save it inside this repo)
- *
- * This Private key is a secret, so be careful with it!
- * Do not save it inside this repo or commit it to git.
- * Do not share it.
- * You may wish to restrict the file permissions on it so only you can access it.
- * For example: "chmod 600 path/to/serviceAccountKey.json".
- * You may also wish to delete it when you're done.
- * Do not share it!
- *
- * In this repo:
- *  - Edit the file env/.env.firebase-download
- *  - Paste in the path to your private key file on your local machine.  For example:
- *      GOOGLE_APPLICATION_CREDENTIALS="path/to/serviceAccountKey.json"
- *
- * Now you can run this script, and it will automatically connect to Firebase using your service account.
- *
- * You can run this with two arguments, studyID and participantID, like this:
- *   npm run firebase:download -- myStudyId myParticipantId
- *
- * This will save data from the last available session for that study and participant.
- * It will also list out all the available sessions.  For example:
- *   Found 3 sessions:
- *     0: 2022-07-26T22:04:55.544Z
- *     1: 2022-07-27T14:16:36.301Z
- *     2: 2022-07-27T19:56:26.229Z
- *
- * If you want to save a different session, you can choose it by passing in the session number as a third argument.  For example:
- *   npm run firebase:download -- myStudyId myParticipantId 0
- *
- * By default, this will save response data in a subfolder of the current folder.
- * The subfolder and file name will be chosen to avoid naming collisions, for example:
- *   ./participant_responses/{session Id}/{participant Id}/{session date}.json
- *
- * Instead of the current folder, you can specify an alternative place to store data.  For example:
- *  npm run firebase:download -- myStudyId myParticipantId latest /path/to/my/data
- *
- * This will result in data saved to
- *  /path/to/my/data/participant_responses/{session Id}/{participant Id}/{session date}.json
+ * Before using this you must set up a Firebase Service account: https://brown-ccv.github.io/honeycomb-docs/docs/firebase#setting-up-a-service-account
+ * For additional details about running the script: https://brown-ccv.github.io/honeycomb-docs/docs/firebase#using-the-download-script
  */
 
 // TODO 172: Refactor to a CJS module
