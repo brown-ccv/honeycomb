068c16a003c854037864583d81ec8eceed361d2a
RobertGemmaJr <rgemma.jr@gmail.com>
2023-08-15 13:29:14 -0400

    wip: Fix build

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-
+import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/App";
+import App from "./App.jsx";
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById("root"));
