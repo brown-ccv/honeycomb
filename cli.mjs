1893599b0d1959f265b4fde4404103c286f64e7e
RobertGemmaJr <rgemma.jr@gmail.com>
2023-08-22 17:15:48 -0400

    ref: Clean up studyIDPrompt functions

--- cli.mjs
+++ cli.mjs
@@ -1,6 +1,8 @@
-import { select, Separator } from "@inquirer/prompts";
+import { input, select, Separator } from "@inquirer/prompts";
 import { cert, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
+
+// TODO: Handling importing code from inside Honeycomb?
 
 /** -------------------- GLOBALS -------------------- */
 
@@ -12,7 +14,7 @@ async function main() {
   // TODO: User should be able to pass command line arguments OR inquirer (especially for task)
   // const [, , ...args] = process.argv;
 
-  const task = await select({
+  const action = await select({
     message: "What would you like to do?",
     choices: [
       {
@@ -26,7 +28,7 @@ async function main() {
     ],
   });
 
-  switch (task) {
+  switch (action) {
     case "download":
       await downloadData();
       break;
@@ -40,7 +42,7 @@ main();
 // `Enter participant id: (* to download all data for the study "${studyID}`)
 // Same for sessions on a participant
 
-/** -------------------- DOWNLOAD -------------------- */
+/** -------------------- DOWNLOAD ACTION -------------------- */
 
 async function downloadData() {
   const deployment = await deploymentPrompt();
@@ -53,11 +55,11 @@ async function downloadData() {
 }
 
 async function downloadDataFirebase() {
-  initializeFirestore();
-  console.log("downloadDataFirebase");
+  const studyID = await studyIDPrompt("download");
+  // console.log("downloadDataFirebase", studyID);
 }
 
-/** -------------------- DELETE -------------------- */
+/** -------------------- DELETE ACTION -------------------- */
 
 async function deleteData() {
   const deployment = await deploymentPrompt();
@@ -71,14 +73,14 @@ async function deleteData() {
 }
 
 async function deleteDataFirebase() {
-  initializeFirestore();
-  console.log("deleteDataFirebase");
+  const studyID = await studyIDPrompt("delete");
+  console.log("deleteDataFirebase", studyID);
 }
 
-/** -------------------- HELPERS -------------------- */
+/** -------------------- PROMPTS -------------------- */
 
 async function deploymentPrompt() {
-  const deployment = await select({
+  const response = await select({
     message: "Which deployment are you using?",
     choices: [
       {
@@ -97,24 +99,56 @@ async function deploymentPrompt() {
     ],
   });
 
-  // TODO: Instantiate Firebase if using Firebase
-  return deployment;
+  if (response === "firebase") {
+    // Initialize Firestore
+    try {
+      const app = initializeApp({ credential: cert("firebase-service-account.json") });
+      FIRESTORE = getFirestore(app);
+    } catch (error) {
+      throw new Error(
+        "Unable to connect to Firebase\n\n" +
+          'Your secret key must be called "firebase-service-account.json" ' +
+          "and stored in the root of your repository.\n" +
+          "More information: https://firebase.google.com/support/guides/service-accounts\n\n" +
+          error.stack
+      );
+    }
+  }
+
+  return response;
+}
+
+async function studyIDPrompt(action) {
+  const response = await input({
+    message: `Which study would you like to ${action}?`,
+    // TODO: Validate studyID is in Firestore
+    validate: async (value) => {
+      const invalid = "Please enter a valid study from your Firestore database";
+      if (!value) return invalid;
+      // TODO: Add validation for studyID existing in Firestore
+      // TODO: Currently an empty document so it doesn't actually exist in the database
+      return true;
+    },
+  });
+
+  return response;
+}
+
+async function participantIDPrompt(action) {
+  const response = await input({
+    message: `Which participant would you like to ${action}?`,
+    // TODO: Validate studyID is in Firestore
+    // TODO: Customize validation message
+    validate: async (value) => {
+      const invalid = "Please enter a valid study from your Firestore database";
+      if (!value) return invalid;
+      // TODO: Add validation for studyID existing in Firestore
+      // TODO: Currently an empty document so it doesn't actually exist in the database
+      return true;
+    },
+  });
+
+  return response;
 }
 
 /** -------------------- FIRESTORE HELPERS -------------------- */
-
-function initializeFirestore() {
-  // Initialize Firestore
-  try {
-    const app = initializeApp({ credential: cert("firebase-service-account.json") });
-    FIRESTORE = getFirestore(app);
-  } catch (error) {
-    throw new Error(
-      "Unable to connect to Firebase\n\n" +
-        'Your secret key must be called "firebase-service-account.json" ' +
-        "and stored in the root of your repository.\n" +
-        "More information: https://firebase.google.com/support/guides/service-accounts\n\n" +
-        error.stack
-    );
-  }
-}
