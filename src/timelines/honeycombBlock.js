e982c562536d4d291e53e0d38f41766003d21104
RobertGemmaJr <rgemma.jr@gmail.com>
2023-08-23 17:27:54 -0400

    feat: Randomize length of fixation trial

--- src/timelines/honeycombBlock.js
+++ src/timelines/honeycombBlock.js
@@ -7,9 +7,14 @@ function createHoneycombBlock(jsPsych, repetitions) {
   // Possible stimuli values to be displayed
   const stimuli = [{ stimulus: "images/blue.png" }, { stimulus: "images/orange.png" }];
 
-  // Note that we use a custom implementation of the fixation trial
-  const fixationTrial = fixation(config, { duration: 650 });
+  // TODO: Pull fixation trial into Honeycomb directly
+  // Create a fixation trial where the duration is of one of the values in trialDurations
+  const trialDurations = [250, 500, 750, 1000, 1250, 1500, 1750, 2000];
+  const fixationTrial = fixation(config, {
+    duration: jsPsych.randomization.sampleWithoutReplacement(trialDurations, 1)[0],
+  });
 
+  // Display one of the stimuli images and wait for a keyboard response of "f" or "j"
   const taskTrial = {
     type: imageKeyboardResponse,
     stimulus: jsPsych.timelineVariable("stimulus"),
@@ -23,6 +28,7 @@ function createHoneycombBlock(jsPsych, repetitions) {
     randomize_order: true,
     repetitions: repetitions,
   };
+  console.log(taskBlock);
   return taskBlock;
 }
 
