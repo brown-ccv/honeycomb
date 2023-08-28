74084685740ac0cf6b9deec760dea30da432c675
RobertGemmaJr <rgemma.jr@gmail.com>
2023-08-28 15:12:57 -0400

    feat: Add interleave function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -95,16 +95,28 @@ const beep = (audioCodes) => {
   o.stop(context.currentTime + 0.4);
 };
 
+/**
+ * Interleave a value before/after every element in an array
+ * @param {Array<any>} arr The original array
+ * @param {any} val The value to interleave inside the array
+ * @param {boolean} addBefore Whether to add val before or after each element in array
+ * @returns The original array with val interleaved between every element
+ */
+function interleave(arr, val, addBefore = true) {
+  return [].concat(...arr.map((n) => (addBefore ? [val, n] : [n, val])));
+}
+
 export {
-  sleep,
-  jitter,
-  jitter50,
-  randomTrue,
+  beep,
   deepCopy,
   formatDollars,
   generateWaitSet,
-  images,
-  startKeypressListener,
   getProlificId,
-  beep,
+  images,
+  interleave,
+  jitter,
+  jitter50,
+  randomTrue,
+  sleep,
+  startKeypressListener,
 };
