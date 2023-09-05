9361ec7578260016beb604770d90f00007960db0
RobertGemmaJr <rgemma.jr@gmail.com>
2023-09-05 14:48:45 -0400

    chore: comments

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
+/**
+ * Displays an error message.
+ *
+ * Note that this error message is only displayed if the task provider has not enabled firebase.
+ * @returns A component that displays an error message.
+ */
 export default function Error() {
   return (
     <div className="centered-h-v">
