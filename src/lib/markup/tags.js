a92e04e5587ffbc5fcb967216c6d4a942fffc6df
RobertGemmaJr <rgemma.jr@gmail.com>
2023-09-13 13:27:33 -0400

    feat: Add h1 and p tags

--- src/lib/markup/tags.js
+++ src/lib/markup/tags.js
@@ -2,14 +2,39 @@
  * This file contains the static markup for various HTML tags.
  * These can be used to wrap your language in specific HTML tags for the JsPSych stimulus.
  */
-// TODO: Base function that injects the tag
 
-function h1(children, attributes = {}) {
+/**
+ * Wraps a given string in the given tag
+ * @param {string} tag The HTML tag to use
+ * @param {string} children The children of the HTML tag
+ * @param {object} attributes HTML attributes to add to the tag
+ * @returns {string} A string containing static HTML
+ */
+function tag(tag, children, attributes = {}) {
   const attributesString = Object.entries(attributes)
     .map(([key, value]) => `${key}="${value}"`)
     .join(" ");
-  console.log(attributes, attributesString);
-  return `<h1 ${attributesString}>${children}</h1>`;
+  return `<${tag} ${attributesString}>${children}</${tag}>`;
 }
 
-export { h1 };
+/**
+ * Wraps a given string in an h1 tag
+ * @param {string} children The children of the HTML tag
+ * @param {object} attributes HTML attributes to add to the tag
+ * @returns {string} A string containing static HTML
+ */
+function h1(children, attributes = {}) {
+  return tag("h1", children, attributes);
+}
+
+/**
+ * Wraps a given string in an paragraph tag
+ * @param {string} children The children of the HTML tag
+ * @param {object} attributes HTML attributes to add to the tag
+ * @returns {string} A string containing static HTML
+ */
+function p(children, attributes = {}) {
+  return tag("p", children, attributes);
+}
+
+export { tag, h1, p };
--- src/trials/holdUpMarker.js
+++ src/trials/holdUpMarker.js
@@ -3,18 +3,16 @@ import htmlButtonResponse from "@jspsych/plugin-html-button-response";
 import { language } from "../config/main";
 import { photodiodeGhostBox } from "../lib/markup/photodiode";
 import { baseStimulus } from "../lib/markup/stimuli";
-import { h1 } from "../lib/markup/tags";
+import { h1, p } from "../lib/markup/tags";
 
 // TODO: Actually check to see if USB is connected? This isn't testing anything?
 function holdUpMarker() {
-  // const eventMarkerMessage = `<h1 style="color: green;">${language.trials.eventMarker.connected}</h1>`;
   const eventMarkerMessage = h1(language.trials.eventMarker.connected, { style: "color: green;" });
-  const stimulus = baseStimulus(eventMarkerMessage, true) + photodiodeGhostBox();
 
   return {
     type: htmlButtonResponse,
-    stimulus,
-    prompt: [`<p>${language.trials.holdUpMarker}</p>`],
+    stimulus: baseStimulus(eventMarkerMessage, true) + photodiodeGhostBox(),
+    prompt: [p(language.trials.holdUpMarker)],
     choices: [language.prompts.continue.button],
   };
 }
