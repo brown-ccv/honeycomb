f601ee6e2ea4d286cea78918f3e674ed06e7cabd
RobertGemmaJr <rgemma.jr@gmail.com>
2023-09-13 15:23:45 -0400

    ref: Cleanup file names

--- src/timelines/main.js
+++ src/timelines/main.js
@@ -4,8 +4,10 @@ import { cameraEnd, cameraStart } from "../trials/camera";
 
 import { createHoneycombTimeline } from "./honeycombTimeline";
 
-// Add your jsPsych options here.
-// Honeycomb will combine these custom options with other options needed by Honyecomb.
+/**
+ * Add your jsPsych options here.
+ * Honeycomb will combine these custom options with other options needed by Honyecomb.
+ */
 const jsPsychOptions = {
   on_trial_finish: function (data) {
     console.log("A trial just ended, here are the latest data:");
@@ -14,20 +16,20 @@ const jsPsychOptions = {
   default_iti: 250,
 };
 
-// Add your jsPsych timeline here.
-// Honeycomb will call this function for us after the subject logs in, and run the resulting timeline.
-// The instance of jsPsych passed in will include jsPsychOptions above, plus other options needed by Honeycomb.
+/**
+ * Add your jsPsych timeline here.
+ * Honeycomb will call this function for us after the subject logs in, and run the resulting timeline.
+ * The instance of jsPsych passed in will include jsPsychOptions above, plus other options needed by Honeycomb.
+ */
 function buildTimeline(jsPsych) {
-  const primaryTimeline = createHoneycombTimeline(jsPsych);
+  const timeline = createHoneycombTimeline(jsPsych);
 
   if (config.USE_CAMERA) {
-    // Add cameraStart after welcome trial
-    primaryTimeline.splice(1, 0, cameraStart(jsPsych));
-    // Add cameraEnd as the last trial
-    primaryTimeline.push(cameraEnd(5000));
+    timeline.unshift(cameraStart(jsPsych)); // Add cameraStart as the first trial
+    timeline.push(cameraEnd(5000)); // Add cameraEnd as the last trial
   }
 
-  return primaryTimeline;
+  return timeline;
 }
 
 export { buildTimeline, jsPsychOptions };
