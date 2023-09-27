c8b6a495e3b9162af82bd3929e1dea08a5d02485
RobertGemmaJr <rgemma.jr@gmail.com>
2023-09-27 16:25:55 -0400

    wip: Clean up index.html

--- public/electron.preload.js
+++ public/electron.preload.js
@@ -1,16 +1,13 @@
-const { config } = require("../src/config/main");
-
-/**
- * Preload script run before the renderer process is loaded
+/** ELECTRON PRELOAD PROCESS
+ *
+ * Note this process has access to bot renderer globals (window and document)
  */
-window.addEventListener("DOMContentLoaded", () => {
-  // const replaceText = (selector, text) => {
-  //   const element = document.getElementById(selector);
-  //   if (element) element.innerText = text;
-  // };
-  console.log(config);
+const { contextBridge } = require("electron");
 
-  for (const dependency of ["chrome", "node", "electron"]) {
-    console.log(`${dependency}-version`, process.versions[dependency]);
-  }
+// As an example, here we use the exposeInMainWorld API to expose the browsers
+// and node versions to the main window.
+// They'll be accessible at "window.versions".
+process.once("loaded", () => {
+  console.log("ONCE");
+  contextBridge.exposeInMainWorld("versions", process.versions);
 });
