394d8d0faa838aeb20076f7b16fb40d67a89123e
RobertGemmaJr <rgemma.jr@gmail.com>
2023-11-21 15:17:11 -0500

    chore: Remove USE_ELECTRON from context bridge
    Variable was not used on the renderer

--- public/electron/preload.js
+++ public/electron/preload.js
@@ -3,7 +3,6 @@ const { contextBridge, ipcRenderer } = require("electron");
 /** Load bridges between the main and renderer processes when the preload process is first loaded */
 process.once("loaded", () => {
   contextBridge.exposeInMainWorld("electronAPI", {
-    USE_ELECTRON: true,
     setConfig: (config) => ipcRenderer.send("setConfig", config),
     getCredentials: () => ipcRenderer.invoke("getCredentials"),
     on_data_update: (data) => ipcRenderer.send("onDataUpdate", data),
