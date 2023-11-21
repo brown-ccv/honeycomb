b72d8c6f96f678be2e7dff0cabdafc965dc50f91
RobertGemmaJr <rgemma.jr@gmail.com>
2023-11-21 15:32:57 -0500

    chore: Comments

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -56,7 +56,7 @@ export default function App() {
         "Task Settings": taskSettings,
       });
 
-      // If on desktop
+      // If running in a native application
       if (config.USE_ELECTRON) {
         await window.electronAPI.setConfig(config); // Pass config to Electron ipcMain
         // TODO 306: Pass trigger to Electron ipcMain
@@ -68,7 +68,8 @@ export default function App() {
 
         setMethod("desktop");
       } else {
-        // If MTURK
+        // If using Mechanical Turk and PsiTurk
+        // TODO: Change config variable to psiturk? That's what user's are really using
         if (config.USE_MTURK) {
           /* eslint-disable */
           window.lodash = _.noConflict();
@@ -77,6 +78,7 @@ export default function App() {
           handleLogin("mturk", turkUniqueId);
           /* eslint-enable */
         } else if (config.USE_PROLIFIC) {
+          // TODO: Remove prolific check - we should always check process.env AND UrlSearchParams
           const pID = getProlificId();
           if (config.USE_FIREBASE && pID) {
             setMethod("firebase");
@@ -86,6 +88,7 @@ export default function App() {
             setIsError(true);
           }
         } else if (config.USE_FIREBASE) {
+          // TODO: There's a difference between PROLIFIC_ID in URL and PID/SID
           // Fill in login fields based on query parameters (may still be blank)
           const query = new URLSearchParams(window.location.search);
           const studyId = query.get("studyID");
@@ -100,7 +103,6 @@ export default function App() {
       }
     }
     setUpHoneycomb();
-    // eslint-disable-next-line
   }, []);
 
   /** VALIDATION FUNCTIONS */
