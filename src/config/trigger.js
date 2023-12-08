e677a33c25725edde5bb91d6d7ab5f119b0033e1
RobertGemmaJr <rgemma.jr@gmail.com>
2023-12-08 16:50:26 -0500

    ref: Clean up trigger codes

--- src/config/trigger.js
+++ src/config/trigger.js
@@ -3,11 +3,10 @@
  * NOTE - these event codes must match what is in public/config/trigger.js
  */
 const eventCodes = {
-  fixation: 1,
-  evidence: 5,
-  show_earnings: 7,
-  test_connect: 32,
-  open_task: 18,
+  fixation: 1, // Fixation trial
+  honeycomb: 2, // Main reaction-time trial for the Honeycomb task
+  open_task: 18, // Opening task for setting up the experiment
+  test_connect: 32, // Initial test connection
 };
 
 // Note that this is module.exports so it can be imported into the electron app
