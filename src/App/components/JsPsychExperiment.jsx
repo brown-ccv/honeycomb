d1e9ffafd50f760185a510075fa990652d55a265
RobertGemmaJr <rgemma.jr@gmail.com>
2023-12-08 14:58:50 -0500

    ref: Remove uneeded keyboard handling

--- src/App/components/JsPsychExperiment.jsx
+++ src/App/components/JsPsychExperiment.jsx
@@ -13,16 +13,7 @@ export default function JsPsychExperiment({
 }) {
   // This will be the div in the dom that holds the experiment.
   // We reference it explicitly here so we can do some plumbing with react, jspsych, and events.
-  const experimentDiv = React.useRef(null);
   const experimentDivId = "experimentWindow";
-
-  // Combine custom options imported from timelines/maine.js, with necessary Honeycomb options.
-  const combinedOptions = {
-    ...jsPsychOptions,
-    display_element: experimentDivId,
-    on_data_update: (data) => dataUpdateFunction(data),
-    on_finish: (data) => dataFinishFunction(data),
-  };
 
   /**
    * Create the instance of JsPsych whenever the studyID or participantID changes,
@@ -38,7 +29,20 @@ export default function JsPsychExperiment({
     // Write the initial record to Firestore
     if (config.USE_FIREBASE) initParticipant(studyID, participantID, startDate);
 
-    const jsPsych = initJsPsych(combinedOptions);
+    const jsPsych = initJsPsych({
+      // Combine custom options (src/timelines/main.js) with necessary Honeycomb options.
+      ...jsPsychOptions,
+      display_element: experimentDivId,
+      on_data_update: (data) => {
+        jsPsychOptions.on_data_update && jsPsychOptions.on_data_update(data); // Call custom on_data_update function (if provided)
+        dataUpdateFunction(data); // Call Honeycomb's on_data_update function
+      },
+      on_finish: (data) => {
+        jsPsychOptions.on_finish && jsPsychOptions.on_finish(data); // Call custom on_finish function (if provided)
+        dataFinishFunction(data); // Call Honeycomb's on_finish function
+      },
+    });
+
     // Add experiment properties into jsPsych directly
     jsPsych.data.addProperties({
       study_id: studyID,
@@ -53,37 +57,11 @@ export default function JsPsychExperiment({
   const timeline = buildTimeline(jsPsych, studyID, participantID);
 
   /**
-   * Callback function used to set up event and lifecycle callbacks to start and stop jspsych.
-   * Inspired by jspsych-react: https://github.com/makebrainwaves/jspsych-react/blob/master/src/index.js
-   * @param {*} e
-   * @returns
-   */
-  const handleKeyEvent = React.useCallback((e) => {
-    if (e.redispatched) return;
-
-    const newEvent = new e.constructor(e.type, e);
-    newEvent.redispatched = true;
-    experimentDiv.current.dispatchEvent(newEvent);
-  }, []);
-
-  /**
    * These useEffect callbacks manage keyboard events between React and the JsPsych experiment
    */
   React.useEffect(() => {
-    window.addEventListener("keyup", handleKeyEvent, true);
-    window.addEventListener("keydown", handleKeyEvent, true);
     jsPsych.run(timeline);
-
-    return () => {
-      window.removeEventListener("keyup", handleKeyEvent, true);
-      window.removeEventListener("keydown", handleKeyEvent, true);
-      try {
-        jsPsych.endExperiment("Ended Experiment");
-      } catch (e) {
-        console.error("Experiment closed before unmount");
-      }
-    };
   });
 
-  return <div id={experimentDivId} ref={experimentDiv} className="App" />;
+  return <div id={experimentDivId} className="App" />;
 }
