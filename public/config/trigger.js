35dcaba6abfdb4a45205f198ef8540a4280858aa
RobertGemmaJr <rgemma.jr@gmail.com>
2023-12-11 10:05:30 -0500

    ref: Defaults productID to undefined

--- public/config/trigger.js
+++ public/config/trigger.js
@@ -9,7 +9,8 @@ const vendorId = "16c0";
  * This value is used for the "teensyduino" trigger box
  * This value can be changed with the environment variable EVENT_MARKER_PRODUCT_ID
  */
-const productId = ""; // TODO: This is not set? Should it be undefined?
+// TODO: This is not set? Should it be undefined?
+const productId = undefined;
 
 /**
  * The COM name of the USB serial device
