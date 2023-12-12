fd61b89c1b4d623cd684139c76328f735f0e0263
RobertGemmaJr <rgemma.jr@gmail.com>
2023-12-12 15:20:40 -0500

    ref: Refactor adjustVolume trial to be a constant

--- src/trials/adjustVolume.js
+++ src/trials/adjustVolume.js
@@ -3,16 +3,15 @@ import { language } from "../config/main";
 import { baseStimulus } from "../lib/markup/stimuli";
 import { h1 } from "../lib/markup/tags";
 
-// TODO: Refactor to constant
-function adjustVolume() {
-  const adjustVolumeMarkup = h1(language.trials.adjustVolume);
+/** Trial that prompts the user to adjust the volume on their computer */
+const adjustVolume = {
+  type: htmlKeyboardResponse,
+  stimulus: () => {
+    const adjustVolumeMarkup = h1(language.trials.adjustVolume);
+    return baseStimulus(adjustVolumeMarkup, true);
+  },
+  prompt: language.prompts.continue.prompt,
+  response_ends_trial: true,
+};
 
-  return {
-    type: htmlKeyboardResponse,
-    stimulus: baseStimulus(adjustVolumeMarkup, true),
-    prompt: language.prompts.continue.prompt,
-    response_ends_trial: true,
-  };
-}
-
-export default adjustVolume;
+export { adjustVolume };
