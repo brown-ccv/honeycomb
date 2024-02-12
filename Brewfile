f1c65a90c8bdb1172bc916d44172b2b0f8743b01
RobertGemmaJr <rgemma.jr@gmail.com>
2024-02-12 09:47:29 -0500

    wip: Delete Honeycomb code for the codespace

--- Brewfile
+++
@@ -1,8 +0,0 @@
-# Brewfile for installing Honeycomb prerequisites
-# Visit https://brown-ccv.github.io/honeycomb-docs/docs/quick_start for info
-brew "git"
-cask "github"
-brew "openjdk"
-cask "visual-studio-code"
-
-# TODO @brown-ccv #278: Python management (miniconda)? [Python is only needed for PsiTurk]
--- src/index.js
+++ src/index.js
@@ -1,10 +1,2 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-import App from "./App/App.jsx";
-
-/** Root of the application.
- *
- * This file renders the React application inside the given location (the browser or Electron)
- */
-ReactDOM.render(<App />, document.getElementById("root"));
+let myNum = 4;
+console.log(myNum);
