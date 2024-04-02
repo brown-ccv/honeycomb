fc217bf5a3e74d44d684466b31388b9b9c79cd70
RobertGemmaJr <rgemma.jr@gmail.com>
2024-04-02 12:34:49 -0400

    build: Install openjdk 17 jre

--- winget.ps1
+++ winget.ps1
@@ -5,7 +5,7 @@ winget install -e --id Git.Git;
 winget install -e --id GitHub.GitHubDesktop;
 winget install -e --id CoreyButler.NVMforWindows
 # winget install -e --id Python.Python.3.11;
-winget install -e --id ojdkbuild.openjdk.17.jdk
+winget install -e --id ojdkbuild.openjdk.17.jre;
 winget install -e --id Microsoft.VisualStudioCode;
 winget install -e --id Microsoft.VisualStudio.2022.Community --override "--add Microsoft.VisualStudio.Workload.NativeDesktop --add Microsoft.VisualStudio.Component.VC.ATLMFC --includeRecommended";
 
