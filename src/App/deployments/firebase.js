05cd145d3ed0b39eefeb2be735de169beb8170ca
RobertGemmaJr <rgemma.jr@gmail.com>
2024-04-04 10:53:39 -0400

    ref: Refactor firebase client to be ALL_CAPS

--- src/App/deployments/firebase.js
+++ src/App/deployments/firebase.js
@@ -4,13 +4,12 @@ import "firebase/compat/firestore";
 
 // Initialize Firebase and Firestore
 firebase.initializeApp({
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  databaseURL: process.env.REACT_APP_databaseURL,
-  projectId: process.env.REACT_APP_projectId ?? "no-firebase",
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID ?? "no-firebase",
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 });
 export const db = firebase.firestore();
 
