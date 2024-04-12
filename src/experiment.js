344794bba84fe0e7dcf898b61cc9dd00ad9cd3c0
RobertGemmaJr <rgemma.jr@gmail.com>
2024-04-12 15:47:07 -0400

    chore: comments

--- src/experiment.js
+++ src/experiment.js
@@ -1,14 +1,21 @@
+// The Basics: Hello World (option 3)
+// https://www.jspsych.org/7.3/tutorials/hello-world/#option-3-using-npm
+
+// Import the code we need
 import { initJsPsych } from "jspsych";
 import htmlKeyboardResponse from "@jspsych/plugin-html-keyboard-response";
+import "jspsych/css/jspsych.css"; // This is custom styling
 
-// Import custom styling
-import "jspsych/css/jspsych.css";
-
+// Initialize the jsPsych instance
+// https://www.jspsych.org/7.3/reference/jspsych/#initjspsych
 const jsPsych = initJsPsych();
 
+// Create a trial object of type htmlKeyboardResponse
+// https://www.jspsych.org/7.3/plugins/html-keyboard-response/
 const trial = {
   type: htmlKeyboardResponse,
   stimulus: "Hello world!",
 };
 
+// Run the jsPsych experiment
 jsPsych.run([trial]);
