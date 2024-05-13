57ece84b848442f22432cba085ea08fca08185b3
RobertGemmaJr <rgemma.jr@gmail.com>
2024-05-13 14:16:10 -0400

    chore: cleanup renderer

--- src/Electron/renderer.js
+++ src/Electron/renderer.js
@@ -1,34 +1,5 @@
 /**
  * This file will automatically be loaded by vite and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/tutorial/application-architecture#main-and-renderer-processes
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
+ * All we need to to do is load the React application.
  */
-
-// TODO: Need to fix the typing here
-console.log(import.meta.env);
-
 import "../index";
-
-console.log('👋 This message is being logged by "renderer.js", included via Vite');
