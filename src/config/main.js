d74c74a013d68639c690fd769159d27ed4582603
RobertGemmaJr <rgemma.jr@gmail.com>
2024-05-13 14:31:43 -0400

    BREAKING_CHANGE: Update scripts

--- src/config/main.js
+++ src/config/main.js
@@ -17,16 +17,6 @@ export const LANGUAGE = language;
 // TODO @brown-ccv #374: Save settings in Firebase
 export const SETTINGS = settings;
 
-// const USE_ELECTRON = window.electronAPI !== undefined; // Whether or not the experiment is running in Electron (local app)
-// const USE_PROLIFIC = getProlificId() !== undefined; // Whether or not the experiment is running with Prolific
-// const USE_FIREBASE = process.env.VITE_FIREBASE === "true"; // Whether or not the experiment is running in Firebase (web app)
-
-// const USE_VOLUME = process.env.VITE_VOLUME === "true"; // Whether or not to use audio cues in the task
-// const USE_CAMERA = process.env.VITE_VIDEO === "true" && USE_ELECTRON; // Whether or not to use video recording
-// // TODO @brown-ccv #341: Remove USE_EEG - separate variables for USE_PHOTODIODE and USE_EVENT_MARKER
-// const USE_EEG = process.env.VITE_USE_EEG === "true" && USE_ELECTRON; // Whether or not the EEG/event marker is available (TODO @brown-ccv: This is only used for sending event codes)
-// const USE_PHOTODIODE = process.env.VITE_USE_PHOTODIODE === "true" && USE_ELECTRON; // whether or not the photodiode is in use
-
 const USE_ELECTRON = window.electronAPI !== undefined; // Whether or not the experiment is running in Electron (local app)
 const USE_PROLIFIC = getProlificId() !== null; // Whether or not the experiment is running with Prolific
 const USE_FIREBASE = import.meta.env.VITE_FIREBASE === "true"; // Whether or not the experiment is running in Firebase (web app)
