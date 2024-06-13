ddaa8fae71a91d0a1c489e5269db2c027dc8b01f
YUUU23 <megan_zheng@brown.edu>
2024-06-13 16:41:17 -0500

    refactor: add p tag to prompt and change back h1 tag for stimulus

--- src/experiment/trials/introduction.js
+++ src/experiment/trials/introduction.js
@@ -1,15 +1,15 @@
 import htmlKeyboardResponse from "@jspsych/plugin-html-keyboard-response";
 
 import { LANGUAGE } from "../../config/main";
-import { div, p } from "../../lib/markup/tags";
+import { div, p, h1 } from "../../lib/markup/tags";
 
 /** Task that displays a introduction message with the photodiode ghost box */
 export const introductionTrial = {
   type: htmlKeyboardResponse,
   response_ends_trial: true,
   stimulus: function () {
-    const introductionMarkup = p(LANGUAGE.trials.introduction);
+    const introductionMarkup = h1(LANGUAGE.trials.introduction);
     return div(introductionMarkup);
   },
-  prompt: LANGUAGE.prompts.continue.prompt,
+  prompt: p(LANGUAGE.prompts.continue.prompt),
 };
