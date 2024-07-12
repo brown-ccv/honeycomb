ec9fa154a58f9a905d7929bd7c3ce8caf5461127
Megan <113795130+YUUU23@users.noreply.github.com>
2024-07-12 13:59:24 -0500

    fix syntax error in firebase

--- src/App/deployments/firebase.js
+++ src/App/deployments/firebase.js
@@ -71,9 +71,8 @@ export async function initParticipant(studyID, participantID, startDate) {
     await experiment.set({
       // TODO @brown-ccv #394: Don't handle any of this here? Let everything be done in jsPsych
 
-    // TODO @brown-ccv #394: Write GIT SHA here
-    // TODO @brown-ccv #394: Store participantID and studyID here, not on each trial
-    await setDoc(experiment, {
+      // TODO @brown-ccv #394: Write GIT SHA here
+      // TODO @brown-ccv #394: Store participantID and studyID here, not on each trial
       start_time: startDate,
       // TODO @brown-ccv #394: app_version and app_platform are deprecated
       app_version: window.navigator.appVersion,
