d00b857e4b4757f140930efc59070d8425b50afd
RobertGemmaJr <rgemma.jr@gmail.com>
2024-07-31 13:12:11 -0400

    chore: add a forge env.d.ts file

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig, mergeConfig } from "vite";
-import { baseConfig } from "./forge.config";
+import { baseConfig } from "./forge.config.mjs";
 
 /** Vite configuration used when running in the browser */
 export default defineConfig(({ root, mode }) =>
