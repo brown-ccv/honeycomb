983a07df930d7fedf342a3f259f18a8caa0e2a8e
RobertGemmaJr <rgemma.jr@gmail.com>
2024-07-31 15:06:26 -0400

    chore: comments

--- vite.main.config.mjs
+++ vite.main.config.mjs
@@ -2,23 +2,6 @@ import { defineConfig, mergeConfig } from "vite";
 
 import { getBuildConfig, getDefineKeys, external, pluginHotRestart } from "./vite.base.config.mjs";
 
-/** @type {(env: import('vite').ConfigEnv<'build'>) => Record<string, any>} */
-const getBuildDefine = ({ command, forgeConfig }) => {
-  const names = forgeConfig.renderer.filter(({ name }) => name != null).map(({ name }) => name);
-  const defineKeys = getDefineKeys(names);
-  const define = Object.entries(defineKeys).reduce((acc, [name, keys]) => {
-    const { VITE_DEV_SERVER_URL, VITE_NAME } = keys;
-    const def = {
-      [VITE_DEV_SERVER_URL]:
-        command === "serve" ? JSON.stringify(process.env[VITE_DEV_SERVER_URL]) : undefined,
-      [VITE_NAME]: JSON.stringify(name),
-    };
-    return { ...acc, ...def };
-  }, {});
-
-  return define;
-};
-
 /** Vite configuration for the main process */
 export default defineConfig((env) => {
   return mergeConfig(getBuildConfig(env), {
@@ -38,3 +21,20 @@ export default defineConfig((env) => {
     },
   });
 });
+
+/** @type {(env: import('vite').ConfigEnv<'build'>) => Record<string, any>} */
+function getBuildDefine({ command, forgeConfig }) {
+  const names = forgeConfig.renderer.filter(({ name }) => name != null).map(({ name }) => name);
+  const defineKeys = getDefineKeys(names);
+  const define = Object.entries(defineKeys).reduce((acc, [name, keys]) => {
+    const { VITE_DEV_SERVER_URL, VITE_NAME } = keys;
+    const def = {
+      [VITE_DEV_SERVER_URL]:
+        command === "serve" ? JSON.stringify(process.env[VITE_DEV_SERVER_URL]) : undefined,
+      [VITE_NAME]: JSON.stringify(name),
+    };
+    return { ...acc, ...def };
+  }, {});
+
+  return define;
+}
