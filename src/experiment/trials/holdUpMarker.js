4ada736617669bdcf2bf9e2c16e0a9713d604e30
Megan <113795130+YUUU23@users.noreply.github.com>
2024-08-06 15:09:34 -0500

    Update src/experiment/trials/holdUpMarker.js
    Co-authored-by: Robert Gemma <38439940+RobertGemmaJr@users.noreply.github.com>

--- src/experiment/trials/holdUpMarker.js
+++ src/experiment/trials/holdUpMarker.js
@@ -3,6 +3,8 @@ import { LANGUAGE } from "../../config/main";
 import { h1 } from "../../lib/markup/tags";
 import { photodiodeGhostBox } from "../../lib/markup/photodiode";
 
+// TODO @brown-ccv #330: Custom extension for EEG - this is initializeTriggerBox
+// TODO @brown-ccv #330: Need to ping the serial part - this isn't doing anything yet
 export const holdUpMarkerTrial = {
   type: htmlButtonResponse,
   stimulus:
